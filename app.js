7e8d4c6bf1c602501b6ff64520a8a14691bac7d2
AnirudhkrishnaS <113175052+AnirudhkrishnaS@users.noreply.github.com>
2022-10-02 14:12:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+
+const app = express()
+// console.log(process.env.SECRET);
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB")
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+userSchema.plugin(encrypt , {secret : process.env.SECRET , encryptedFields : ['password']});
+
+const USER = new mongoose.model("User", userSchema)
+
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+
+app.post("/register", (req, res) => {
+    const newUser = new USER({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets")
+        }
+    })
+})
+
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // console.log(username);
+
+
+    USER.findOne({ email: username }, (err, foundUser) => {
+        // console.log(foundUser);
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(foundUser);
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets")
+                }
+            }
+        }
+    })
+})
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('server started');
+})
